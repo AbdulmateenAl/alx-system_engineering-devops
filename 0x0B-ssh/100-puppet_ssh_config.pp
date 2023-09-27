1286689d362834961ec3469cd6046024044e54ce
AbdulmateenAl <alabiabdulmateen94@gmail.com>
2023-09-27 15:37:56 -0700

    wrote a puppet script that changes my configuration file to allow clients connect to server without password

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Using puppet to change my configuration file
+file { 'etc/ssh/ssh.config':
+	ensure  => present
+	content => "
+
+	#SSH client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+}
